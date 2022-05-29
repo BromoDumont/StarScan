ca0a0603b9c9d2cb8bc8aeec962ec7d754317ce8
Rivaldo Segundo <rivaldoserafim05@hotmail.com>
2022-05-29 09:46:10 -0300

    29-05-2022-0.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,23 @@
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 using System.IO;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private PlayerScript _PlayerScript;
+    [Tooltip ("Conecta o script 'PlayerScript' com o script 'GameManager'.")] [SerializeField] 
+    private PlayerScript _PlayerScript;
+
+    [Tooltip ("Objeto que armazena os elementos da UI quando o jogo está pausado")] [SerializeField]
+    private GameObject pauseObj;
+
+    [Tooltip ("Componente da imagem do botão de pause.")] [SerializeField]
+    private Image pauseBtnImageComp;
+
+    [Tooltip ("Armazena os sprites do botão de pause.")] [SerializeField]
+    private Sprite[] pauseBtnSprite;
 
     public int maxPoints;
 
@@ -17,17 +28,21 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        //inGamePointsTxt.text = _PlayerScript.roundPoint.ToString();
+        //inGamePointsTxt.text = _PlayerScript.roundPoints.ToString();
     }
 
     public void Pause()
     {
         if (Time.timeScale > 0)
         {
+            pauseBtnImageComp.sprite = pauseBtnSprite[0];
+            pauseObj.SetActive(true);
             Time.timeScale = 0;
         }
         else
         {
+            pauseBtnImageComp.sprite = pauseBtnSprite[1];
+            pauseObj.SetActive(false);
             Time.timeScale = 1;
         }
     }
@@ -37,14 +52,14 @@ public class GameManager : MonoBehaviour
         //plObj.SetActive(false);
         //deathScreenObj.SetActive(true);
         //deathMaxPoints.text = maxPoints.ToString();
-        //deathRoundPoints.text = _PlayerScript.roundPoint.ToString();
+        //deathRoundPointss.text = _PlayerScript.roundPoints.ToString();
     }
 
     public void GameRestart()
     {
         SaveData();
         SceneManager.LoadScene(0);
-        _PlayerScript.roundPoint = 0;
+        _PlayerScript.roundPoints = 0;
         //deathScreenObj.SetActive(false);
     }
 
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,36 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerScript : MonoBehaviour
 {
-    [Tooltip ("Conecta o script 'GameManager' com o script 'PlayerScript'")] [SerializeField] 
+    [Tooltip ("Conecta o script 'GameManager' com o script 'PlayerScript'.")] [SerializeField] 
     private GameManager _GameManager;
 
-    [Tooltip ("Objeto que fica no local onde o player conectou no gancho")] [SerializeField]
+    [Tooltip ("Objeto que fica no local onde o player conectou no gancho.")] [SerializeField]
     private GameObject hookCacheObj;
 
-    [Tooltip ("Prefab do gancho")] [SerializeField]
+    [Tooltip ("Prefab do gancho.")] [SerializeField]
     private GameObject hookPrefab;
 
-    [Tooltip ("Objeto da câmera")] [SerializeField]
+    [Tooltip ("Objeto da câmera.")] [SerializeField]
     private GameObject cameraObj;
 
-    [Tooltip ("Define a velocidade angular do player")] [SerializeField]
+    [Tooltip ("Define a velocidade angular do player.")] [SerializeField]
     private float plAngularVel;
+
+    [Tooltip ("Caixa de texto que mostra a quantidade de pontos do player.")] [SerializeField]
+    private TextMeshProUGUI pointsTxtBox;
 
     [Header ("Debug Data----------------------------")]
     [Space]
 
-    [Tooltip ("Armazena a quantidade de pontos da rodada atual")]
-    public int roundPoint;
+    [Tooltip ("Armazena a quantidade de pontos da rodada atual.")]
+    public int roundPoints;
+
+    [Tooltip ("Armazena a informação se o player está ou não conectado em um gancho.")]
     public bool plOnHook;
-    public float hookXAxis;
 
     #region Variaveis Privadas
         private Rigidbody2D plBody; //Componente que aplica física ao player.
         private LineRenderer hookLine; //Componente que fara a linha entre os ganchos e o player.
         private GameObject hookObj; //Objeto do gancho.
+        private float cameraXAxis; //Valor do eixo X da camêra
     #endregion
 
     void Awake()
@@ -41,15 +47,16 @@ public class PlayerScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        cameraObj.transform.position = new Vector3(hookXAxis + 4, 0, -10);
+        pointsTxtBox.text = roundPoints.ToString();
+        cameraObj.transform.position = new Vector3(cameraXAxis + 4, 0, -10);
         hookLine.SetPosition(0, plBody.position);
 
         if (plOnHook == true)
         {
             hookLine.SetPosition(1, hookObj.transform.position);
-            if (hookXAxis < hookObj.transform.position.x)
+            if (cameraXAxis < hookObj.transform.position.x)
             {
-                hookXAxis += 0.5f;
+                cameraXAxis += 0.5f;
             }
         }
         else
@@ -60,17 +67,64 @@ public class PlayerScript : MonoBehaviour
 
     public void AddPoint()
     {
-        roundPoint++;
+        roundPoints++;
 
-        if (_GameManager.maxPoints < roundPoint)
+        if (_GameManager.maxPoints < roundPoints)
         {
-            _GameManager.maxPoints = roundPoint;
+            _GameManager.maxPoints = roundPoints;
         }
     }
 
     void GetHook()
     {
         NewHook();
+
+        if (plAngularVel > 0 & plAngularVel < 301)
+        {
+            plAngularVel += roundPoints/4;
+        }
+
+        if (plAngularVel < 0 & plAngularVel > -301)
+        {
+            plAngularVel -= roundPoints/4;
+        }
+
+        if (plAngularVel > 0)
+        {
+            if (plAngularVel < 300)
+            {
+                plAngularVel += roundPoints/4;
+            }
+            else
+            {
+                if (plAngularVel < 500)
+                {
+                    plAngularVel += roundPoints/8;
+                }
+                else
+                {
+                    plAngularVel += roundPoints/16;
+                }
+            }
+        }
+        else
+        {
+            if (plAngularVel > -300)
+            {
+                plAngularVel -= roundPoints/4;
+            }
+            else
+            {
+                if (plAngularVel > -500)
+                {
+                    plAngularVel -= roundPoints/8;
+                }
+                else
+                {
+                    plAngularVel -= roundPoints/16;
+                }
+            }
+        }
         AddPoint();
 
         if (plBody.velocity.y > 0 & plAngularVel > 0 || plBody.velocity.y < 0 & plAngularVel < 0)
@@ -99,7 +153,7 @@ public class PlayerScript : MonoBehaviour
 
     void NewHook()
     {
-        Instantiate(hookPrefab, new Vector2(hookObj.transform.position.x + 16.5f, Random.Range(-20, 15)), Quaternion.identity);
+        Instantiate(hookPrefab, new Vector2(hookObj.transform.position.x + 16.5f, Random.Range(-17, 15)), Quaternion.identity);
     }
 
     void OnTriggerEnter2D(Collider2D collider)
